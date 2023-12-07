58f863de6c298e6868294b2956cd08502e83bd49
Ali <alihosseinifaratliara@gmail.com>
2023-12-07 14:33:40 +0330

    All routes are practically ready, but they need some work to become operational

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import passport from 'passport';
 import jsonwebtoken from "jsonwebtoken";
 import { EnvVars } from "./utils/EnvVars";
 import { initSession } from "./utils/initServer/initSession";
 import { initPassport } from "./utils/initServer/initPassport";
 import { initJwt } from "./utils/initServer/initJwt";
+import prisma from "./utils/prisma";
+import multer from "multer";
+import { GetObjectCommand, PutObjectAclCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import path from "path";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 declare global {
   namespace Express {
@@ -18,6 +23,7 @@ const app = express();
 initSession(app);
 initPassport(app);
 initJwt(app);
+const upload = multer({ storage: multer.memoryStorage() });
 
 // this should be a post req
 app.get("/login", passport.authenticate("google", { scope: ['profile', 'email'] }));
@@ -28,9 +34,164 @@ app.get("/login/callback",
     return res.json(jsonwebtoken.sign({ id }, EnvVars.JWT_SECRET));
   });
 
-app.get("/protected", passport.authenticate("jwt", { session: false }), async (req, res) => {
-  console.log(req.user)
-  return res.json("you got access!");
+// cursor pagination in the future
+app.get("/feedbacks", async (req, res) => {
+  // https://medium.com/@aliammariraq/prisma-exclude-with-typesafety-8484ea6d0c42
+  const feedbacks = await prisma.feedbacks.findMany({
+    select: {
+      id: true,
+      title: true,
+      description: true,
+      user: {
+        select: {
+          name: true,
+          color: true
+        }
+      },
+      _count: {
+        select: {
+          votes: true
+        }
+      }
+    }
+  });
+  return res.json(
+    feedbacks
+  );
+});
+
+// validation with zod, only zod.
+app.post("/feedbacks", [
+  passport.authenticate("jwt", { session: false }),
+  upload.single("attachment")
+], async (req: Request<{}, {}, {
+  title: string;
+  description: string;
+}>, res: Response) => {
+  let fileUrl: string | undefined = undefined;
+  if (req.file) {
+    const name = path.parse(req.file.originalname);
+    const key = name.name + Date.now() + name.ext;
+    const client = new S3Client({
+      region: "default",
+      endpoint: EnvVars.S3_ENDPOINT,
+      credentials: {
+        accessKeyId: EnvVars.S3_ACCESS_KEY,
+        secretAccessKey: EnvVars.S3_SECRET_KEY
+      }
+    });
+    await client.send(new PutObjectCommand({
+      Body: req.file.buffer,
+      Bucket: EnvVars.S3_BUCKET_NAME,
+      Key: key
+    }));
+    fileUrl = await getSignedUrl(client, new GetObjectCommand({
+      Bucket: EnvVars.S3_BUCKET_NAME,
+      Key: key
+    }));
+  }
+  await prisma.feedbacks.create({
+    data: {
+      userId: req.user?.id!,
+      title: req.body.title,
+      description: req.body.description,
+      attachmentUrl: fileUrl,
+    }
+  })
+  return res.send("success");
+});
+
+// guard against the existence of the feedback
+app.get("/feedbacks/:feedbackId", async (req, res) => {
+  return res.json(
+    prisma.feedbacks.findUnique({
+      where: {
+        id: req.params.feedbackId
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        createdAt: true,
+        user: {
+          select: {
+            name: true,
+            color: true
+          }
+        },
+        comments: {
+          select: {
+            id: true,
+            feedbackId: true,
+            content: true,
+            createdAt: true,
+            user: {
+              select: {
+                name: true,
+                color: true
+              }
+            },
+          }
+        },
+        _count: {
+          select: {
+            votes: true
+          }
+        }
+      }
+    })
+  )
+});
+
+app.post("/comment", passport.authenticate("jwt", { session: true }), async (req: Request<{}, {}, {
+  feedbackId: string,
+  content: string
+}>, res) => {
+  await prisma.comments.create({
+    data: {
+      userId: req.user?.id!,
+      feedbackId: req.body.feedbackId,
+      content: req.body.content
+    }
+  });
+  return res.json("comment posted");
+});
+
+app.post("/vote", passport.authenticate("jwt", { session: true }), async (req: Request<{}, {}, {
+  feedbackId: string
+}>, res) => {
+  // check for uniqueness
+  await prisma.votes.create({
+    data: {
+      userId: req.user?.id!,
+      feedbackId: req.body.feedbackId
+    }
+  });
+  return res.json("vote casted");
+});
+
+app.get("/me", passport.authenticate("jwt", { session: true }), async (req, res) => {
+  return res.json(prisma.users.findUnique({
+    where: {
+      id: req.user?.id!
+    }
+  }));
+});
+
+app.post("/me", passport.authenticate("jwt", { session: true }), async (req: Request<{}, {}, {
+  name: string,
+  color: string
+}>, res) => {
+  await prisma.users.update({
+    where: {
+      id: req.user?.id!
+    },
+    data: {
+      name: req.body.name,
+      color: req.body.color
+    }
+  });
+  return res.json("profile updated");
 });
 
 app.listen(EnvVars.PORT || 3000, () => {
--- apps/api/src/utils/EnvVars.ts
+++ apps/api/src/utils/EnvVars.ts
@@ -9,6 +9,10 @@ export class EnvVars {
   public static CLIENT_SECRET = envVar.get("CLIENT_SECRET").required().asString();
   public static COOKIE_SECRET = envVar.get("COOKIE_SECRET").required().asString();
   public static JWT_SECRET = envVar.get("JWT_SECRET").required().asString();
+  public static S3_ENDPOINT = envVar.get("S3_ENDPOINT").required().asString();
+  public static S3_BUCKET_NAME = envVar.get("S3_BUCKET_NAME").required().asString();
+  public static S3_ACCESS_KEY = envVar.get("S3_ACCESS_KEY").required().asString();
+  public static S3_SECRET_KEY = envVar.get("S3_SECRET_KEY").required().asString();
   public static PORT = envVar.get("PORT").required().asPortNumber();
   public static HOST = envVar.get("HOST").required().asString();
 }
